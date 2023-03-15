63a35cd12b45ab508d46aa9b54738ec38b3dd6c0
Belal <mohammadvns121@gmail.com>
2023-03-16 02:10:19 +0530

    bold_3rd item

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var HeadTittle = document.querySelector("#header-title");
-HeadTittle.style.borderBottom = "solid 3px #000";
+// var HeadTittle = document.querySelector("#header-title");
+// HeadTittle.style.borderBottom = "solid 3px #000";
 
 var items = document.querySelectorAll(".list-group-item");
+items[2].style.backgroundColor = "green"
 
-// for(let i=0; i<items.length; i++){
-//     items[i].textContent.style.fontWeigth = "bold"
-// }
+for(let i=0; i<items.length; i++){
+ items[i].style.fontWeight = "bold";
+}
 
-var Tittle = document.querySelector("h2");
-    // Tittle.textContent = "Hello";
-    // console.log(Tittle)
-    Tittle.style.fontWeight = "bold"
-    Tittle.style.color = "green";
+
+
+
+
+
+
