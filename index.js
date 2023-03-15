d12eb948fd0b587d3b197145bd0fe9413b181a22
Belal <mohammadvns121@gmail.com>
2023-03-16 00:51:08 +0530

    second_form code chand

---
+++ index.js
@@ -0,0 +1,14 @@
+var HeadTittle = document.querySelector("#header-title");
+HeadTittle.style.borderBottom = "solid 3px #000";
+
+var items = document.querySelectorAll(".list-group-item");
+
+// for(let i=0; i<items.length; i++){
+//     items[i].textContent.style.fontWeigth = "bold"
+// }
+
+var Tittle = document.querySelector("h2");
+    // Tittle.textContent = "Hello";
+    // console.log(Tittle)
+    Tittle.style.fontWeight = "bold"
+    Tittle.style.color = "green";
