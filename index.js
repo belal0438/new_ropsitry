05ff8239fe2cfe804a05a6d69592f7e121de4f58
Belal <mohammadvns121@gmail.com>
2023-03-16 02:50:59 +0530

    getelemetByTag

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // var HeadTittle = document.querySelector("#header-title");
 // HeadTittle.style.borderBottom = "solid 3px #000";
 
-var items = document.querySelectorAll(".list-group-item");
-items[2].style.backgroundColor = "green"
-
+var items = document.getElementsByTagName("li");
 for(let i=0; i<items.length; i++){
- items[i].style.fontWeight = "bold";
+    items[i].style.backgroundColor = "#f4f4f4";
 }
+
+
+
+// UlElement = document.getElementById("items");
+// li = document.createElement("li");
+// li.textContent = "Hello word";
+// li.style.fontWeight = "bold";
+// UlElement.append(li);
+
+// var items = document.getElementsByTagName("li");
+// for(let i=0; i<items.length; i++){
+//     items[i].style.backgroundColor = "#f4f4f4";
+// }
+
 
 
 
